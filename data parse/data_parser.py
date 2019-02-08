ca1c3f0ee6741138bc4a2ea1c714709ceb00dca9
HaiyueChen <chenhaiyue2010@hotmail.com>
2019-02-08 13:46:45 +0100

    first project

---
+++ data parse/data_parser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys, ast
+
+if __name__ == "__main__":
+    
+    output_file_name = sys.argv[1]
+    print(output_file_name)
+    with open(output_file_name, "w") as f:
+        for line in sys.stdin:
+            try:
+                output = int(line)
+            except Exception as e:
+                f.write("\n"+line.rstrip())
+                continue
+
+            f.write(","+str(output))
+    
+        f.write("\n")
